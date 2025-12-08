6951fe1e336578d4f5d48313f4e8df21cfd80cf1
Mehmet Seyfi <mehmetcanseyfi@gmail.com>
2025-12-08 23:40:14 +0300

    chore: add initial project files

---
+++ README.md
@@ -0,0 +1,1 @@
+# ![logo](https://raw.githubusercontent.com/kodladev/renit/refs/heads/main/.github/assets/symbol.svg) Renit
---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    ignores: [
+      '**/.DS_Store',
+      '**/node_modules',
+    ],
+  },
+];
