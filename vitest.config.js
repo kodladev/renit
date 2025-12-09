d4d2d249c6d04e6ec264631938ddd3dd463bc6aa
Mehmet Seyfi <mehmetcanseyfi@gmail.com>
2025-12-10 02:11:51 +0300

    feat: update CI to install vitest and remove jsdom usage

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,5 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['packages/**/test/**/*.test.js'],
-    environmentMatchGlobs: [['packages/**/test/**/*.client.test.js', 'jsdom']],
   },
 });
